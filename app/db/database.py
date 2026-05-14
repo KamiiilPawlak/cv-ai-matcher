c4b36cbe8f2bee506b2e286b539879b440d77d2c
KamiiilPawlak <kami.patryk.pawlak@gmail.com>
2026-05-14 23:08:48 +0200

    feat:  created db

---
+++ app/db/database.py
@@ -0,0 +1,18 @@
+from typing import Generator
+
+from models.cv import RawCV, RawJobOfferts
+from sqlmodel import Session, SQLModel, create_engine
+
+from ..core.config import settings
+
+engine = create_engine(settings.DATABASE_URL, echo=True)
+
+
+def init_db() -> None:
+    _ = [RawCV, RawJobOfferts]
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
