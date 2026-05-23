8e3cb5053fd81bdd1066f6eaf2a0b39653e67d8c
KamiiilPawlak <kami.patryk.pawlak@gmail.com>
2026-05-23 19:33:08 +0200

    chore: initialize logging system in main application entrypoint

---
+++ backend/app/core/logger.py
@@ -0,0 +1,42 @@
+# ai_matcher/core/logger.py
+import os
+import sys
+
+from loguru import logger  # type: ignore
+
+
+def setup_logging() -> None:
+
+    logger.remove()
+
+    env: str = os.getenv("APP_ENV", "dev").lower()
+
+    log_level: str = os.getenv("LOG_LEVEL", "DEBUG" if env == "dev" else "INFO").upper()
+
+    if env == "prod" or env == "ci":
+        logger.add(
+            sys.stdout,
+            level=log_level,
+            serialize=True,
+        )
+    else:
+        log_format = (
+            "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+            "<level>{level: <8}</level> | "
+            "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
+            "<level>{message}</level>"
+        )
+
+        logger.add(sys.stdout, level=log_level, format=log_format, colorize=True)
+
+        logger.add(
+            "logs/errors.log",
+            level="ERROR",
+            format=log_format,
+            rotation="10 MB",
+            retention="7 days",
+            compression="zip",
+            colorize=True,
+            backtrace=True,
+            diagnose=True,
+        )
--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,10 @@ from contextlib import asynccontextmanager
 from typing import AsyncGenerator, Dict
 
 from fastapi import FastAPI
+from loguru import logger
 
 from app.api.v1.router import api_router
+from app.core.logger import setup_logging  # type: ignore
 
 from .db.database import init_db
 
@@ -16,6 +18,9 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     print("Zamykanie aplikacji..")
 
 
+setup_logging()
+
+
 app = FastAPI(title="CV AI Matcher", lifespan=lifespan)
 
 
@@ -25,3 +30,6 @@ app.include_router(api_router, prefix="/api/v1")
 @app.get("/")
 def root() -> Dict[str, str]:
     return {"message": "docker :D"}
+
+
+logger.info("Progam został uruchomiony.....")
